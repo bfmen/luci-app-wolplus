b57f011f4b17201612b685f139b4bda4ad3484e6
VIKING <viking.yfy@qq.com>
2025-01-06 23:25:44 +0800

    插件更新

---
+++ luci-app-wol/htdocs/luci-static/resources/view/wol.js
@@ -0,0 +1,30 @@
+'use strict';
+'require view';
+'require form';
+'require fs';
+
+return view.extend({
+	render: function(data) {
+		var m, s, o, mac;
+
+		m = new form.Map('etherwake', _('WakeUp On Lan'), _('WakeUp On Lan'));
+		s = m.section(form.GridSection, 'target');
+		s.nodescriptions = true;
+		s.anonymous = true;
+		s.addremove = true;
+
+		o = s.(form.Value, 'name', _('Name'), _('the Name of device'));
+
+		mac = s.option(form.Value, 'mac', _('MAC'), _('the MAC of device'));
+
+		o = s.option(form.Button, '_apply', _('WakeUp'));
+		o.editable = true;
+		o.modalonly = false;
+		o.inputstyle = 'apply';
+		o.onclick = function(ev, section_id) {
+			var val=mac.cfgvalue(section_id);
+			return fs.exec('/usr/bin/etherwake',['-D','-i','br-lan',val]).then(function(res){alert(res.stdout+res.stderr)}).catch(function(err) {alert(err)});
+		}
+		return m.render();
+	},
+});
